b6ac5dbef424b886371ce38ce2be2e5ffc20cab5
Valmir Sales <valmir_salesgama@hotmail.com>
2024-07-10 17:31:27 -0300

    Erro, uma classe não pode herdar mais de uma clas

--- ExerciciosJava/herançaMultiplaEOProblemaDoDiamante/src/devices/ComboDevice.java
+++ ExerciciosJava/herançaMultiplaEOProblemaDoDiamante/src/devices/ComboDevice.java
@@ -1,6 +1,6 @@
 package devices;
 
-public class ComboDevice extends  Scanner {
+public class ComboDevice extends  Scanner, Printer {
 
 	public ComboDevice(String serialNumber) {
 		super(serialNumber);
