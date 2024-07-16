25f509b8bd48bba747a6ecd8b042af02ca19bc73
Valmir Sales <valmir_salesgama@hotmail.com>
2024-07-16 19:19:48 -0300

    Interface Consumer

---
+++ ExerciciosJava/interfaceConsumer/src/application/Program.java
@@ -0,0 +1,28 @@
+package application;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Locale;
+
+import entities.Product;
+import util.PriceUpdate;
+
+public class Program {
+
+	public static void main(String[] args) {
+		
+		Locale.setDefault(Locale.US);
+		List<Product> list = new ArrayList<>();
+		
+		list.add(new Product("TV", 900.00));
+		list.add(new Product("Mouse", 50.00));
+		list.add(new Product("Tablet", 350.50));
+		list.add(new Product("HD Case", 80.90));
+		
+		// List forEach recebendo uma instância no consumer
+		list.forEach(new PriceUpdate());
+		
+		// Usando um reference method para o println no consumer
+		list.forEach(System.out::println);
+	}
+}
---
+++ ExerciciosJava/interfaceConsumer/src/entities/Product.java
@@ -0,0 +1,50 @@
+package entities;
+
+public class Product {
+
+	private String name;
+	private Double price;
+	
+	// Construtor default
+	public Product() {
+	}
+
+	// Construtor com argumentos
+	public Product(String name, Double price) {
+		this.name = name;
+		this.price = price;
+	}
+
+	// Getters e Setters
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public Double getPrice() {
+		return price;
+	}
+
+	public void setPrice(Double price) {
+		this.price = price;
+	}
+	
+	// Método static
+	public static boolean staticProductPredicate(Product p) {
+		return p.getPrice() >= 100.0;
+	}
+	
+	// Método boolean não Static
+		public boolean nonStaticProductPredicate() {
+			return price >= 100.0;
+		}
+	
+	// Método toString
+	@Override
+	public String toString() {
+		return name + ", " + String.format("%.2f", price);
+	}
+}
