1c9cfcbc45c8bda17894a589274682231e8c1985
Valmir Sales <valmir_salesgama@hotmail.com>
2024-07-16 19:51:24 -0300

    Expressão lambda declarada

--- ExerciciosJava/interfaceConsumer/src/application/Program.java
+++ ExerciciosJava/interfaceConsumer/src/application/Program.java
@@ -3,9 +3,9 @@ package application;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Locale;
+import java.util.function.Consumer;
 
 import entities.Product;
-import util.PriceUpdate;
 
 public class Program {
 
@@ -19,8 +19,13 @@ public class Program {
 		list.add(new Product("Tablet", 350.50));
 		list.add(new Product("HD Case", 80.90));
 		
-		// List forEach fazendo uma referência ao método não static no consumer
-		list.forEach(Product::nonStaticPriceUpdate);
+		double factor = 1.1; // Valor de atualização de preço que pode ser lida pelo usuário
+		
+		// Variável do tipo Consumer recebendo uma função lambida
+		Consumer<Product> cons = p -> p.setPrice(p.getPrice() * factor);
+
+		// List forEach recebendo como argumento a variável do tipo consumer
+		list.forEach(cons);
 		
 		// Usando um reference method para o println no consumer
 		list.forEach(System.out::println);
