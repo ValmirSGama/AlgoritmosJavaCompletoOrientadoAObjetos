7e15a8830545dfb5cb423f5109608350c7e2a71d
Valmir Sales <valmir_salesgama@hotmail.com>
2024-07-16 12:09:43 -0300

    Expressão lâmbida declarada

--- ExerciciosJava/predicadoInterfaceFuncional/src/application/Program.java
+++ ExerciciosJava/predicadoInterfaceFuncional/src/application/Program.java
@@ -3,9 +3,9 @@ package application;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Locale;
+import java.util.function.Predicate;
 
 import entities.Product;
-import util.ProductPredicate;
 
 public class Program {
 
@@ -19,8 +19,13 @@ public class Program {
 		list.add(new Product("Tablet", 350.00));
 		list.add(new Product("HD Case", 80.00));
 		
-		// Lista recebendo uma referência para método não Static 
-		list.removeIf(Product::nonStaticProductPredicate);
+		double min = 100.0; // valor para ser parametrizado na função lâmbida, vantagem que pode ser definida pelo usuário 
+		
+		// Expressão lâmbida recebendo como predicado uma função anônima
+		Predicate<Product> pred = p -> p.getPrice() >= min;
+		
+		// Lista recebendo como referência a variável pred
+		list.removeIf(pred);
 		
 		for(Product p : list) {
 			System.out.println(p);
