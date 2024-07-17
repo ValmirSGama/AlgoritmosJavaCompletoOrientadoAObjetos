d094bc34b7c23818f45096334bb968e4561a4603
Valmir Sales <valmir_salesgama@hotmail.com>
2024-07-16 22:33:17 -0300

    Expressão lambda inline

--- ExerciciosJava/interfaceFunction/src/application/Program.java
+++ ExerciciosJava/interfaceFunction/src/application/Program.java
@@ -3,7 +3,6 @@ package application;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Locale;
-import java.util.function.Function;
 import java.util.stream.Collectors;
 
 import entities.Product;
@@ -20,11 +19,8 @@ public class Program {
 		list.add(new Product("Tablet", 350.50));
 		list.add(new Product("HD Case", 80.90));
 		
-		// Function recebendo uma função lambda
-		Function<Product, String> func = p -> p.getName().toUpperCase();
-		
 		// convertendo cada elemento da lista original para letras maiúsculas e depois coletando os resultados em uma nova lista
-		List<String> names = list.stream().map(func).collect(Collectors.toList());
+		List<String> names = list.stream().map(p -> p.getName().toUpperCase()).collect(Collectors.toList());
 		
 		names.forEach(System.out::println);
 	}
